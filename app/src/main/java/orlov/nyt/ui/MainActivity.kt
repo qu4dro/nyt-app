4359ba88fb3b0bd356d67fa35818de597f9c3e28
Orlov Peter <qu4dro.sl@gmail.com>
2022-09-07 12:00:13 +0800

    Dependencies and empty navbar added

--- app/src/main/java/orlov/nyt/ui/MainActivity.kt
+++ app/src/main/java/orlov/nyt/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package orlov.nyt
+package orlov.nyt.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import orlov.nyt.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
