1c3188763e7ad0e5e047c96c92671b576441ed63
Orlov Peter <qu4dro.sl@gmail.com>
2022-09-09 14:21:58 +0800

    Refactored activity

--- app/src/main/java/orlov/nyt/utils/Extensions.kt
+++ app/src/main/java/orlov/nyt/utils/Extensions.kt
@@ -1,9 +1,11 @@
 package orlov.nyt.utils
 
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
@@ -13,5 +15,17 @@ fun <T> Fragment.collectLatestLifecycleFlow(flow: Flow<T>, collect: suspend (T) 
         viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
             flow.collectLatest(collect)
         }
+    }
+}
+
+fun BottomNavigationView.show() {
+    if (visibility == View.GONE || visibility == View.INVISIBLE) {
+        visibility = View.VISIBLE
+    }
+}
+
+fun BottomNavigationView.hide() {
+    if (visibility == View.VISIBLE) {
+        visibility = View.GONE
     }
 }
